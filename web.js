581c4fe2ef2b2481524320f80b4951e0ae95b130
Caitlin.Hall2 <Caitlin.Hall2@target.com>
2014-03-21 11:22:36 -0500

    init

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('<div style="blue">Hello World! Caitlin</div>');
+  res.send('<div style="color:blue">Hello World! Caitlin</div>');
 
 });
 
