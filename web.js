ded0ef51dbbdf48982c2d6a3e8006f5564af5846
Caitlin.Hall2 <Caitlin.Hall2@target.com>
2014-03-21 11:21:11 -0500

    init

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('<div style="blue">Hello World! Caitlin</div>');
+
 });
 
 var port = Number(process.env.PORT || 5000);
